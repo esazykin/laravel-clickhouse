2649d638a2e2004a842e56121ab7eba64f648301
Babichev Maxim <info@babichev.net>
2020-06-28 21:27:41 +0300

    Update ClickHouseServiceProvider.php

--- src/ClickHouseServiceProvider.php
+++ src/ClickHouseServiceProvider.php
@@ -16,10 +16,14 @@ class ClickHouseServiceProvider extends ServiceProvider
         /** @var DatabaseManager $db */
         $db = $this->app->get('db');
 
-        $db->extend('clickhouse', function ($config, $name) {
+        $db->extend('clickhouse-ext', function ($config, $name) {
             $config['name'] = $name;
+            $connection = new Connection($config);
+            if ($this->app->bound('events')) {
+                $connection->setEventDispatcher($this->app['events']);
+            }
 
-            return new Connection($config);
+            return $connection;
         });
 
         Model::setConnectionResolver($db);
